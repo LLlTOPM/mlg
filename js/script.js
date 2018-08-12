f6246decbbf785bf186353a34ba033f0c2c88a40
LLlTOPM <llltopm666@gmail.com>
2018-08-13 02:57:52 +0500

    add sunglass

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var main = document.getElementById("fullpage"),
     timerIDforFallingMlgStaff,
     timerIDforComicText,
+    timerIDforSunglass,
     countMlgStaff = 21; //count of png +1
 
 var data = {
@@ -53,7 +54,7 @@ var data = {
         },
         {
             "fullName": "Павел Павлович",
-            "pic": "lena.jpg",
+            "pic": "3.jpg",
             "desription": "самое жирное описание. самое жирное описание. самое жирное описание." +
             "самое жирное описание. самое жирное описание. самое жирное описание." +
             "самое жирное описание. самое жирное описание. самое жирное описание." +
@@ -76,7 +77,7 @@ var data = {
         },
         {
             "fullName": "Семен Семенович",
-            "pic": "grisha.jpg",
+            "pic": "1.jpg",
             "desription": "самое жирное описание. самое жирное описание. самое жирное описание." +
             "самое жирное описание. самое жирное описание. самое жирное описание." +
             "самое жирное описание. самое жирное описание. самое жирное описание." +
@@ -107,15 +108,23 @@ var data = {
         output +=
 
             "<div class='section'>" +
-            "<div class='comicTextBlock'><img src='pic/comicText/wow.png'></div>" +
-            "<div class='fixer'>"+
-            "<div class='mlgBlock' id='mlgBlockId " + i + "'></div></div>" +
-            "<div class='subject'>" +
-            "<div class='info'>" +
-            "<div class='fullName'>" + data.persons[i].fullName + "</div>" +
-            "<div class='description'>" + data.persons[i].desription + "</div></div>" +
-            "<div class='photo'><img src='pic/photo/" + data.persons[i].pic + "' class='pic' id='picPerson" + i + "'></div>" +
-            "</div></div>";
+                "<div class='comicTextBlock'><img src='pic/comicText/wow.png'></div>" +
+                "<div class='fixer'>"+
+                    "<div class='mlgBlock' id='mlgBlockId " + i + "'></div>" +
+                "</div>" +
+                "<div class='subject'>" +
+                    "<div class='info'>" +
+                        "<div class='fullName'>" + data.persons[i].fullName + "</div>" +
+                        "<div class='description'>" + data.persons[i].desription + "</div>" +
+                    "</div>" +
+                    "<div class='photo'>" +
+                        "<div class='sunglassBlock'>" +
+                            "<img src='pic/mlgStaff/sunglass.png'>" +
+                        "</div>" +
+                        "<img src='pic/photo/" + data.persons[i].pic + "' class='pic' id='picPerson" + i + "'>" +
+                    "</div>" +
+                "</div>" +
+            "</div>";
     }
     main.innerHTML = output;
 
@@ -155,7 +164,7 @@ function loadComicText() {
 
 
 function hideMlgBlock () {
-    console.log('hide');
+    console.log('hide mlgBlock');
     $('.fp-section.active').find('.mlgBlock').css("display", "none");
 }
 
@@ -169,6 +178,11 @@ function showComicText() {
 function hideComicText() {
     console.log("hide comic text");
     $('.fp-section.active').find('.comicTextBlock').css("display", "none");
+}
+
+function hideSunglass() {
+    console.log("hide sunglass");
+    $('.fp-section.active').find('.sunglassBlock').css("display", "none");
 }
 
 function randomInteger() {
@@ -185,18 +199,23 @@ $(document).ready(function () {
         afterLoad: function (anchorLink, index, slideIndex, direction, nextSlideIndex) {
 
             var slideNumber = $('.fp-section.active').index();  // if have not only sections, has been see fucking index 1,3,5,7.
-            console.log(slideNumber);
-
 
             $('.fp-section.active').find('.mlgBlock').css("display", "flex");
             $('.fp-section.active').find('.mlgBlock').children().addClass('mlgActive0');
 
+            $('.fp-section.active').find('.sunglassBlock').css("display", "flex");
+            $('.fp-section.active').find('.sunglassBlock').children().addClass('sunglassForPerson'+ slideNumber);
+
             timerIDforFallingMlgStaff = setTimeout(hideMlgBlock, 8000);
+            timerIDforSunglass = setTimeout(hideSunglass,5000);
             timerIDforComicText = setInterval(showComicText, 5000);
         },
         onLeave: function (anchor, index) {
             clearTimeout(timerIDforFallingMlgStaff);
+            clearTimeout(timerIDforSunglass);
             clearInterval(timerIDforComicText);
+
+            $('.fp-section.active').find('.sunglassBlock').css("display", "none");
             $('.fp-section.active').find('.mlgBlock').css("display", "none");
             $('.fp-section.active').find('.comicTextBlock').css("display", "none");
         }
